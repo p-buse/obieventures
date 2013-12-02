0eac1a4f27a3d58e27cacc9e7d3c1a1157b5375e
root <root@obieventures.(none)>
2013-12-02 17:36:34 -0500

    About to abstract out displayed adventure

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+before_filter :http_basic_authenticate
+
+def http_basic_authenticate
+  authenticate_or_request_with_http_basic do |username, password|
+    username == "99username" && password == "password99"
+  end
+end
   protect_from_forgery with: :exception
 end
